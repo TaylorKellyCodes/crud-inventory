0f10a10be4165e48e3fdfd224de02fff4da46b90
Taylor <Taylorkellycodes@gmail.com>
2022-09-27 15:30:02 -0400

    Setting things up

---
+++ README.md
@@ -0,0 +1,8 @@
+# Run
+
+Bring up terminal and navigate to the folder containing 
+`npm install`
+then
+`npm start`
+Then
+go to localhost:2121 in your browser.
---
+++ models/Comment.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdBy: {
+    type: String,
+    ref: "User"
+  },
+  createdById: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
